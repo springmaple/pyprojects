e7b6b14bc1cb7759904488f1d77bb1ad8ce3515a
springmaple <springmaple@live.com.my>
2016-12-28 21:15:35 +0800

    update

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,3 +1,43 @@
+def bubble_sort(m):
+    """a.k.a sinking sort"""
+    for i in reversed(range(len(m))):
+        for j in range(i):
+            k = j + 1
+            l, r = m[j], m[k]
+            if l > r:
+                m[j], m[k] = r, l
+
+    return m
+
+
+def selection_sort():
+    ...
+
+
+def insertion_sort(m):
+    for i, x in enumerate(m):
+        for j in reversed(range(i)):
+            y = m[j]
+            if x < y:
+                m[j], m[j + 1] = x, y
+                continue
+            break
+
+    return m
+
+
+def quick_sort(m):
+    ...
+
+
+def bucket_sort(m):
+    ...
+
+
+def radix_sort(m):
+    ...
+
+
 def merge_sort(m):
     mlen = len(m)
     if mlen <= 2:
@@ -19,27 +59,3 @@ def merge_sort(m):
 
     merged += l + r
     return merged
-
-
-def insertion_sort(m):
-    for i, x in enumerate(m):
-        for j in reversed(range(i)):
-            y = m[j]
-            if x < y:
-                m[j], m[j + 1] = x, y
-                continue
-            break
-
-    return m
-
-
-def bubble_sort(m):
-    """a.k.a sinking sort"""
-    for i in reversed(range(len(m))):
-        for j in range(i):
-            k = j + 1
-            l, r = m[j], m[k]
-            if l > r:
-                m[j], m[k] = r, l
-
-    return m
